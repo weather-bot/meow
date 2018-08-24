6879f315b7ebe4536fb0fc5a055d4df250fefa82
tigercosmos <phy.tiger@gmail.com>
2018-08-25 03:00:56 +0800

    update weather info

--- src/weather_info.rs
+++ src/weather_info.rs
@@ -1,8 +1,10 @@
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WeatherInfo {
     pub title: String,
+    pub location: String,
     pub time: String,
     pub temp: f64,
     pub humd: f64,
     pub overview: String,
+    pub overview2: String,
 }
