51121200ea1b5a5127531b1cc08ceb0ee42f654a
tigercosmos <phy.tiger@gmail.com>
2018-08-29 03:47:38 +0800

    draw_corner: new design

--- src/draw.rs
+++ src/draw.rs
@@ -42,31 +42,37 @@ fn get_text_width(text: &str, height: u32) -> u32 {
 //    | 800 x 130    Title        |
 //    |                           |
 //    +---------------------------+
-//    |                300 x 350  |
-//    |       Kitty    +----------+
-//    |       Image    | time     |
-//    |                | temp     |
-//    |                | humd     |
-//    |                | overview |
+//    |    Kitty Image            |
+//    |                200x370    |
+//    |200x130         +----------+
+//    +------+         | temp     |
+//    | loc  |         | humd     |
+//    | time |         | overview |
 //    +----------------+----------+
 
 const IMG_WIDTH: u32 = 800;
 const IMG_HEIGHT: u32 = 800;
 const TITLE_HEIGHT: u32 = 130;
-const CORNER_INFO_WIDTH: u32 = 300;
-const CORNER_INFO_HEIGHT: u32 = 350;
+const INFO_BLOCK_WIDTH: u32 = 200;
+const INFO_BLOCK_HEIGHT: u32 = 120;
+const LEFT_CORNER_HEIGHT: u32 = 130;
+const RIGHT_CORNER_HEIGHT: u32 = INFO_BLOCK_HEIGHT * 3 + 10;
 
 pub fn draw_corner(
     image_path: &str,
     weather_info: &WeatherInfo,
     output_path: &str,
 ) {
-    let background_color = Rgba([146u8, 146u8, 146u8, 100u8]);
+    let background_color = Rgba([94u8, 94u8, 94u8, 100u8]);
     let text_color = Rgba([255u8, 255u8, 255u8, 255u8]);
 
-    // Weather Info Rect
-    let pos_x = IMG_WIDTH - CORNER_INFO_WIDTH;
-    let mut pos_y = IMG_HEIGHT - CORNER_INFO_HEIGHT;
+    // font type
+    let font =
+        Vec::from(include_bytes!("../font/NotoSansCJKtc-Medium.ttf") as &[u8]);
+    let font = FontCollection::from_bytes(font)
+        .unwrap()
+        .into_font()
+        .unwrap();
 
     // Open image and crop to the correct size
     let mut origin_img = image::open(image_path).unwrap();
@@ -78,86 +84,143 @@ pub fn draw_corner(
         ImageBuffer::from_fn(IMG_WIDTH, TITLE_HEIGHT, |_, _| background_color);
     imageops::overlay(&mut origin_img, &bg_img_title, 0, 0);
 
-    // Weather infomation background
-    let bg_img_info =
-        ImageBuffer::from_fn(CORNER_INFO_WIDTH, CORNER_INFO_HEIGHT, |_, _| {
+    // Left corner background
+    let bk_img_info_left =
+        ImageBuffer::from_fn(INFO_BLOCK_WIDTH, LEFT_CORNER_HEIGHT, |_, _| {
             background_color
         });
-    imageops::overlay(&mut origin_img, &bg_img_info, pos_x, pos_y);
+    imageops::overlay(
+        &mut origin_img,
+        &bk_img_info_left,
+        0,
+        IMG_HEIGHT - BOTTOM_INFO_HEIGHT,
+    );
 
-    // font type
-    let font =
-        Vec::from(include_bytes!("../font/NotoSansCJKtc-Medium.ttf") as &[u8]);
-    let font = FontCollection::from_bytes(font)
-        .unwrap()
-        .into_font()
-        .unwrap();
-
-    // weather information text
-    let humd_str = format!("濕度:{}%", weather_info.humd);
-    let temp_str = format!("溫度:{}°C", weather_info.temp);
-    let overview_str = format!("天氣:{}", weather_info.overview);
+    // Right corner background
+    let bk_img_info_right =
+        ImageBuffer::from_fn(INFO_BLOCK_WIDTH, RIGHT_CORNER_HEIGHT, |_, _| {
+            background_color
+        });
+    imageops::overlay(
+        &mut origin_img,
+        &bk_img_info_right,
+        IMG_WIDTH - INFO_BLOCK_WIDTH,
+        IMG_HEIGHT - RIGHT_CORNER_HEIGHT,
+    );
 
     // title
     let text_height = TITLE_HEIGHT - 20;
     let width = get_text_width(&weather_info.title, text_height);
+    check_width(width, IMG_WIDTH, "title");
     draw_text_mut(
         &mut origin_img,
         text_color,
-        ((IMG_WIDTH - width) as f32 / 4.0).round() as u32,
+        ((IMG_WIDTH - width) as f32 / 2.0).round() as u32,
         0,
         Scale::uniform(text_height as f32),
         &font,
         &weather_info.title,
     );
 
-    // time
-    let pos_x = pos_x + 20;
-    let text_height = 80;
+    // Location
+    let pos_x = 10;
+    let pos_loc_y = IMG_HEIGHT - BOTTOM_INFO_HEIGHT;
+    let location_font_size = 80;
+    let width = get_text_width(&weather_info.location, location_font_size);
+    check_width(width, INFO_BLOCK_WIDTH, "location");
     draw_text_mut(
         &mut origin_img,
         text_color,
         pos_x,
-        pos_y,
-        Scale::uniform(text_height as f32),
+        pos_loc_y,
+        Scale::uniform(location_font_size as f32),
+        &font,
+        &weather_info.location,
+    );
+
+    // Time
+    let pos_time_y = pos_loc_y + location_font_size;
+    let time_font_size = 50;
+    let width = get_text_width(&weather_info.time, time_font_size);
+    check_width(width, INFO_BLOCK_WIDTH, "time");
+    draw_text_mut(
+        &mut origin_img,
+        text_color,
+        pos_x,
+        pos_time_y,
+        Scale::uniform(time_font_size as f32),
         &font,
         &weather_info.time,
     );
 
-    // temperature
-    pos_y += text_height + 5;
-    draw_text_mut(
-        &mut origin_img,
-        text_color,
-        pos_x,
-        pos_y,
-        Scale::uniform(text_height as f32),
-        &font,
-        &temp_str,
-    );
+    // Humidity
+    let pos_x = IMG_WIDTH - INFO_BLOCK_WIDTH + 10;
+    let pos_y = IMG_HEIGHT - RIGHT_CORNER_HEIGHT + 10;
+    let water_drop_icon = image::open("img/water_drop.png").unwrap().to_rgba();
+    imageops::overlay(&mut origin_img, &water_drop_icon, pos_x, pos_y);
 
-    // humidity
-    pos_y += text_height + 5;
+    let pos_humd_x = pos_x + 48 + 10; // 48 is icon width
+    let humd_font_size = 80.0;
+    let humd_str = format!("{}%", &weather_info.humd);
+    check_value(weather_info.humd, "humidity");
     draw_text_mut(
         &mut origin_img,
         text_color,
-        pos_x,
+        pos_humd_x,
         pos_y,
-        Scale::uniform(text_height as f32),
+        Scale::uniform(humd_font_size),
         &font,
         &humd_str,
     );
 
-    // overview
-    pos_y += text_height + 5;
+    // Temperature
+    let pos_y = pos_y + INFO_BLOCK_HEIGHT;
+    let thermometer_icon =
+        image::open("img/thermometer.png").unwrap().to_rgba();
+    imageops::overlay(&mut origin_img, &thermometer_icon, pos_x, pos_y);
+
+    let pos_tmp_x = pos_x + 40 + 10; // 40 is icon width
+    let temp_font_size = 80.0;
+    let temp_str = format!("{}℃", &weather_info.temp);
+    check_value(weather_info.temp, "temperature");
+    draw_text_mut(
+        &mut origin_img,
+        text_color,
+        pos_tmp_x,
+        pos_y,
+        Scale::uniform(temp_font_size),
+        &font,
+        &temp_str,
+    );
+
+    // Overview 1
+    let pos_y = pos_y + INFO_BLOCK_HEIGHT;
+    let ov1_font_size = 60;
+    let width = get_text_width(&weather_info.overview, ov1_font_size);
+    check_width(width, INFO_BLOCK_WIDTH, "overview");
     draw_text_mut(
         &mut origin_img,
         text_color,
         pos_x,
         pos_y,
-        Scale::uniform(text_height as f32),
+        Scale::uniform(ov1_font_size as f32),
         &font,
-        &overview_str,
+        &weather_info.overview,
+    );
+
+    // Overview 2
+    let pos_y = pos_y + ov1_font_size;
+    let ov2_font_size = 60;
+    let width = get_text_width(&weather_info.overview2, ov2_font_size);
+    check_width(width, INFO_BLOCK_WIDTH, "overview2");
+    draw_text_mut(
+        &mut origin_img,
+        text_color,
+        pos_x,
+        pos_y,
+        Scale::uniform(ov2_font_size as f32),
+        &font,
+        &weather_info.overview2,
     );
 
     let _ = origin_img.save(Path::new(output_path)).unwrap();
